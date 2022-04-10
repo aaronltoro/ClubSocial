9eb404fac01aa5292050bb8dc8039aa6c2f1ca0b
JesusPR00 <93611119+JesusPR00@users.noreply.github.com>
2022-04-10 16:15:28 +0200

    V0.2.1

---
+++ application/views/Insert_empresa.php
@@ -0,0 +1,24 @@
+<div>
+    <form id="insert_empresa">
+        <div class="row">
+            <div class="col-sm-8 offset-2 mt-3">
+                <input class="form-control" type="text" name="nombre" placeholder="Nombre">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8 offset-2 mt-3">
+                <input class="form-control" type="text" name="cif" placeholder="CIF">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8 offset-2 mt-3">
+                <input class="form-control" type="text" name="direccion" placeholder="Dirección">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2 offset-5 mt-3 mb-3 d-flex justify-content-center">
+                <button class="btn btn-success btn_add" type="button" onclick="add_empresa()">Añadir</button>
+            </div>
+        </div>
+    </form>
+</div>
