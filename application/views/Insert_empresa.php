d5c2faa7e4e7c5119efbf60357330d8bfe1a6cec
JaviSoriano95 <javisoriano3@gmail.com>
2022-04-11 21:07:19 +0200

    version 0.3.1

--- application/views/Insert_empresa.php
+++ application/views/Insert_empresa.php
@@ -11,8 +11,35 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-8 offset-2 mt-3">
-                <input class="form-control" type="text" name="direccion" placeholder="Dirección">
+        <div class="col-sm-2 offset-2 mt-3">
+                <select class="form-control" name="nsedes" id="nsedes">
+                    <option value="0">NºSedes</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                </select>
+            </div>
+            <!--Script que pinta tantos input de dirección como numero haya puesto el usuario en el select-->
+            <script>
+                document.getElementById("nsedes").addEventListener('change',pinta_inputs);
+
+                function pinta_inputs(){
+                    nsedes = document.getElementById("nsedes").value;
+                    res= "";
+
+                    for(i=0;i<nsedes;i++){
+                        res += '<div class="col-sm-12 mb-2 p-0"><input class="form-control" type="text" name="direccion'+(i+1)+'" placeholder="Dirección '+(i+1)+'"></div>';
+                    }
+
+                    document.getElementById("res").innerHTML = res;
+                }
+            </script>
+            <div class="col-sm-5 offset-1 mt-3" id='res'>
+
             </div>
         </div>
         <div class="row">
